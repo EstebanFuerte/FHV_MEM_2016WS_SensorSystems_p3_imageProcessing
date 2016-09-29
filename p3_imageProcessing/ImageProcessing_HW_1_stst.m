9577276c9b4e011a9bd7cbe938c6a44782af916b
Stefan Stark <sst5281@students.fhv.at>
2016-09-29 08:56:04 +0200

    hw1_update
    add comments and change view of gradient (quiever)

--- p3_imageProcessing/ImageProcessing_HW_1_stst.m
+++ p3_imageProcessing/ImageProcessing_HW_1_stst.m
@@ -13,7 +13,10 @@ I = im2double(I);
 figure; imshow(I);              % shows the image
 
 %% Smooth Image (Gaussian Filter)
-F = fspecial('Gaussian',11,3);
+F = fspecial('Gaussian',5,2);  % in time domains the calculation time
+                                % depends square time with the size of the
+                                % filter. eh filter of size leads to 25
+                                % calculations per pixel.
 figure; surf(F);                % plots 3-D colored surface
 I1 = imfilter(I,F);
 figure; imshow(I1);             % shows the smoothed image
@@ -29,10 +32,20 @@ subplot(2,1,1); imshow(gy,[]);
 subplot(2,1,2); imshow(gx,[]);
 
 %% Show gradient as Vector
-figure; quiver(I,gy); zoom(8)
-figure; quiver(I,gx); zoom(8)
+figure; quiver(gx,gy)           % without the smoothing, the gradients
+                                % would be much more chaotic
+zoom(4);
 
 %% Show Magnitude of Gradient
-M = abs(gx)+abs(gy);
+M = abs(gx)+abs(gy);            
+M = sqrt(gx.*gx+gy.*gy);        % another method to calculate the gradient
 figure; imshow(M,[])
+colormap(copper)
+colorbar;
+
+
+%% notes
+% .* operation -> Elementwise operation
+% [2 1] [7 2] vektor multiplikation is not possible, we would like to mult.
+% elementwise so the result should be: [2*7 1*2]=[14 2]
 
